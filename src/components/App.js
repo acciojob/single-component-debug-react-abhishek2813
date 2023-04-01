47e532d0967787f2152b774d8b36f64fc39baf82
abhishek2813 <abhi93158879@gmail.com>
2023-04-01 21:03:43 +0530

    solve

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
 
-export default function App() {
+function App() {
   return (
-   
-        <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-    
+    <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
   )
 }
 
+export default App
